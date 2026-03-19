ff8354d85354ecebc9b7d512600d8cef1d337247
LunaDiegoLeo <diegolhl678@gmail.com>
2026-03-19 11:44:28 -0600

    HU1 - implementar función para agregar tareas

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,24 @@
 let tasks = [];
+
+
+function addTask() {
+    const input = document.getElementById("taskInput");
+    const text = input.value;
+
+    if (text === "") {
+        alert("Escribe una tarea");
+        return;
+    }
+
+    const task = {
+        id: Date.now(),
+        text: text,
+        completed: false
+    };
+
+    tasks.push(task);
+
+    input.value = "";
+
+    renderTasks();
+}
