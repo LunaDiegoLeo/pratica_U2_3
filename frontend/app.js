f9a07b05804e3f80141ca913ece206b8118d9b25
LunaDiegoLeo <diegolhl678@gmail.com>
2026-03-19 11:56:43 -0600

    H1: mostrar tareas

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,12 @@
 let tasks = [];
+
+function renderTasks() {
+    const list = document.getElementById("taskList");
+    list.innerHTML = "";
+
+    tasks.forEach(task => {
+        const li = document.createElement("li");
+        li.textContent = task.text;
+        list.appendChild(li);
+    });
+}
